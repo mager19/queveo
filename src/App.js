217d83cded27dbc2364b4781307466656211725b
mager19 <mager19@gmail.com>
2022-05-20 15:37:24 -0500

    tailwind added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<div className="container mx-auto">
+				<div className="flex flex-wrap px-4">
+					<div className="menu w-full py-3 flex justify-between border-b border-winter">
+						<div className="logo">
+							<h1 class="text-2xl font-bold text-winter">
+								Que veo Pues
+							</h1>
+						</div>
+
+						<div className="results">
+							<h3 class="text-xl font-bold text-winter">
+								Has seleccionado{" "}
+								<span class="text-orange">12 </span>Shows y
+								tienen <span class="text-orange">334</span>{" "}
+								Episodios
+							</h3>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			colors: {
+				navy: "#112b3c",
+				steel: "#205375",
+				orange: "#f66b0e",
+				winter: "#efefef",
+			},
+
+			fontFamily: { redHat: "Red Hat Display" },
+		},
+	},
+	plugins: [],
+};
